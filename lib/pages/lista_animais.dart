6c3c5f960e978699adcefd1cef82eef45375de50
DilciHettwer <dilcylopes@gmail.com>
2021-07-18 10:27:12 -0300

    Inclusão button e correção
    Inclusão do botão de cadastrar na lista de animais e correção no preenchimento  dos dados da page user

--- lib/pages/lista_animais.dart
+++ lib/pages/lista_animais.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:petshop/controller/animal_controller.dart';
 import 'package:petshop/model/animal_model.dart';
+import 'package:petshop/pages/cadastro.dart';
 
 class ListaAnimais extends StatefulWidget {
   @override
@@ -14,7 +15,6 @@ class _ListaAnimaisState extends State<ListaAnimais> {
   @override
   Widget build(BuildContext context) {
     return SliverList(
-      
         delegate: SliverChildListDelegate(<Widget>[
       FutureBuilder<List<AnimalM>>(
         future: _animalController.buscarAnimais(),
@@ -23,7 +23,6 @@ class _ListaAnimaisState extends State<ListaAnimais> {
             case ConnectionState.none:
             case ConnectionState.waiting:
               return Center(
-                
                 child: CircularProgressIndicator(),
               );
               break;
@@ -31,55 +30,61 @@ class _ListaAnimaisState extends State<ListaAnimais> {
             case ConnectionState.done:
               if (index.data != null && index.data.isNotEmpty) {
                 return ListView.builder(
-                  
                   scrollDirection: Axis.vertical,
                   shrinkWrap: true,
                   itemCount: index.data.length,
                   itemBuilder: (context, key) {
-                    AnimalM  animal = List<AnimalM>.from(index.data)[key];
+                    AnimalM animal = List<AnimalM>.from(index.data)[key];
                     return cardItem(animal);
                     
                   },
+                  
                 );
               }
           }
           return null;
         },
       ),
-    ]));
+     SizedBox(height: 15.0),
+        Column(
+          children: [
+            FloatingActionButton.extended(
+              backgroundColor: const Color(0xff0277bd),
+              foregroundColor: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Cadastro()),
+                );
+              },
+              icon: Icon(Icons.pets),
+              label: Text('Cadastrar'),
+            ),
+             SizedBox(height: 15.0),
+          ],
+        ),
+    ]
+    ));
   }
 }
 
 Widget cardItem(AnimalM animal) {
-   return Card(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20))),
-          width: 400,
-          height: 400,
-          child: Image.asset("assets/banho.jpg"),
+  return Card(
+      child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+    Container(
+        child: Column(children: [
+      ListTile(
+        leading: Image.asset("assets/banho.jpg"),
+        //leading:  Image.network(animal.arquivo),
+        title: Text('Nome: ' + animal.nome),
+        subtitle: Text(
+          animal.descricao,
+          style: TextStyle(color: Colors.black.withOpacity(0.6)),
         ),
-        Container(
-          padding: EdgeInsets.all(10),
-          child: Text(animal.nome),
-        ),]));}
-  //  return Card(
-    //   child: Column(
-    //     crossAxisAlignment: CrossAxisAlignment.end,
-    // mainAxisSize: MainAxisSize.min,
-    // children: <Widget>[
-    //   ListTile(
-        
-        
-    //    // leading: Image.asset("assets/banho.jpg"),
-    //     title: Text('Nome: ' + animal.nome),
-    //     subtitle: Text(
-    //       animal.raca,
-    //       style: TextStyle(color: Colors.black.withOpacity(0.6)),
-    //     ),
-    //   )]));
+      ),
+    ])),
+  ]));
+}
+  
   //return Text(animal.nome);
   //TODO criar um exibicao melhorada do animal com sua respectiva imagem
